4864da3632da73b7dffc91226abe7ae4a0c2ec0a
Kevin <58185783+KNolan531@users.noreply.github.com>
2021-03-29 06:32:55 -0400

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./components/Card";
+import Box from "./components/Box";
+
+
+function Home () {
+  return (
+    <>
+      <Box /> 
+      
+    </>
+  )
+}
+
+export default Home;
